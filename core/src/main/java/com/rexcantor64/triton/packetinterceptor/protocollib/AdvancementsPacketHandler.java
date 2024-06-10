sheduxdev/Triton

commit ad100e06b4f1fc08ec1f67e2b233e32a7402c12d

fix(spigot): advancement translation in MC 1.20.6

core/src/main/java/com/rexcantor64/triton/packetinterceptor/protocollib/AdvancementsPacketHandler.java
@@ -2,6 +2,7 @@ package com.rexcantor64.triton.packetinterceptor.protocollib;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.FuzzyReflection;
 import com.comphenix.protocol.reflect.accessors.Accessors;
 import com.comphenix.protocol.reflect.accessors.FieldAccessor;
 import com.comphenix.protocol.reflect.accessors.MethodAccessor;
@@ -51,20 +52,21 @@ public class AdvancementsPacketHandler extends PacketHandler {
             SERIALIZED_ADVANCEMENT_CLASS = null;
             ADVANCEMENT_DISPLAY_FIELD = null;
         }
-        val advancementDataPlayerClass = MinecraftReflection.getMinecraftClass("server.AdvancementDataPlayer", "AdvancementDataPlayer");
+        val advancementDataPlayerClass = MinecraftReflection.getMinecraftClass("server.PlayerAdvancements", "server.AdvancementDataPlayer", "AdvancementDataPlayer");
         ENTITY_PLAYER_ADVANCEMENT_DATA_PLAYER_FIELD = Accessors.getFieldAccessor(MinecraftReflection.getEntityPlayerClass(), advancementDataPlayerClass, true);
-        ADVANCEMENT_DATA_PLAYER_REFRESH_METHOD = Accessors.getMethodAccessor(advancementDataPlayerClass, "b", MinecraftReflection.getEntityPlayerClass());
+        ADVANCEMENT_DATA_PLAYER_REFRESH_METHOD = Accessors.getMethodAccessor(advancementDataPlayerClass, "flushDirty", MinecraftReflection.getEntityPlayerClass());
 
-        val advancementDataWorldClass = MinecraftReflection.getMinecraftClass("server.AdvancementDataWorld", "AdvancementDataWorld");
+        val advancementDataWorldClass = MinecraftReflection.getMinecraftClass("server.ServerAdvancementManager", "server.AdvancementDataWorld", "AdvancementDataWorld");
         CRAFT_SERVER_GET_SERVER_METHOD = Accessors.getMethodAccessor(MinecraftReflection.getCraftBukkitClass("CraftServer"), "getServer");
-        MINECRAFT_SERVER_GET_ADVANCEMENT_DATA_METHOD = Accessors.getMethodAccessor(Arrays.stream(MinecraftReflection.getMinecraftServerClass().getMethods())
-                .filter(m -> m.getReturnType() == advancementDataWorldClass).findAny()
-                .orElseThrow(() -> new RuntimeException("Unable to find method getAdvancementData([])")));
+        MINECRAFT_SERVER_GET_ADVANCEMENT_DATA_METHOD = Accessors.getMethodAccessor(
+                FuzzyReflection.fromClass(MinecraftReflection.getMinecraftServerClass())
+                        .getMethodByReturnTypeAndParameters("getAdvancements", advancementDataWorldClass)
+        );
 
         if (MinecraftVersion.NETHER_UPDATE.atOrAbove()) { // 1.16+
             // MC 1.16+
             // Loading of achievements requires an AdvancementDataWorld method
-            ADVANCEMENT_DATA_PLAYER_LOAD_FROM_ADVANCEMENT_DATA_WORLD_METHOD = Accessors.getMethodAccessor(advancementDataPlayerClass, "a", advancementDataWorldClass);
+            ADVANCEMENT_DATA_PLAYER_LOAD_FROM_ADVANCEMENT_DATA_WORLD_METHOD = Accessors.getMethodAccessor(advancementDataPlayerClass, "reload", advancementDataWorldClass);
         } else {
             // MC 1.12-1.15
             // Loading of achievements only needs the method to be called without any parameters

core/src/main/java/com/rexcantor64/triton/wrappers/WrappedAdvancementDisplay.java
@@ -19,8 +19,8 @@ import java.util.Optional;
  */
 public class WrappedAdvancementDisplay extends AbstractWrapper {
 
-    private static Class<?> ADVANCEMENT_DISPLAY = MinecraftReflection.getMinecraftClass("advancements.AdvancementDisplay", "AdvancementDisplay");
-    private static Class<?> ADVANCEMENT_FRAME_TYPE_CLASS = MinecraftReflection.getMinecraftClass("advancements.AdvancementFrameType", "AdvancementFrameType");
+    private static Class<?> ADVANCEMENT_DISPLAY = MinecraftReflection.getMinecraftClass("advancements.DisplayInfo", "advancements.AdvancementDisplay", "AdvancementDisplay");
+    private static Class<?> ADVANCEMENT_FRAME_TYPE_CLASS = MinecraftReflection.getMinecraftClass("advancements.AdvancementType", "advancements.AdvancementFrameType", "AdvancementFrameType");
     private static ConstructorAccessor ADVANCEMENT_DISPLAY_CONSTRUTOR = Accessors.getConstructorAccessor(
             ADVANCEMENT_DISPLAY,
             MinecraftReflection.getItemStackClass(), // icon

commit 926bfc40bf9ef76488950efefb1ce8b1441a8f84

fix(spigot): regression in advancement translation pre 1.20.6
Regression introduced in commit ad100e0

core/src/main/java/com/rexcantor64/triton/packetinterceptor/protocollib/AdvancementsPacketHandler.java
@@ -54,10 +54,13 @@ public class AdvancementsPacketHandler extends PacketHandler {
         }
         val advancementDataPlayerClass = MinecraftReflection.getMinecraftClass("server.PlayerAdvancements", "server.AdvancementDataPlayer", "AdvancementDataPlayer");
         ENTITY_PLAYER_ADVANCEMENT_DATA_PLAYER_FIELD = Accessors.getFieldAccessor(MinecraftReflection.getEntityPlayerClass(), advancementDataPlayerClass, true);
-        ADVANCEMENT_DATA_PLAYER_REFRESH_METHOD = Accessors.getMethodAccessor(advancementDataPlayerClass, "flushDirty", MinecraftReflection.getEntityPlayerClass());
+        ADVANCEMENT_DATA_PLAYER_REFRESH_METHOD = Accessors.getMethodAccessor(
+                FuzzyReflection.fromClass(advancementDataPlayerClass)
+                        .getMethodByReturnTypeAndParameters("flushDirty", void.class, MinecraftReflection.getEntityPlayerClass())
+        );
 
         val advancementDataWorldClass = MinecraftReflection.getMinecraftClass("server.ServerAdvancementManager", "server.AdvancementDataWorld", "AdvancementDataWorld");
-        CRAFT_SERVER_GET_SERVER_METHOD = Accessors.getMethodAccessor(MinecraftReflection.getCraftBukkitClass("CraftServer"), "getServer");
+        CRAFT_SERVER_GET_SERVER_METHOD = Accessors.getMethodAccessor(MinecraftReflection.getCraftServer(), "getServer");
         MINECRAFT_SERVER_GET_ADVANCEMENT_DATA_METHOD = Accessors.getMethodAccessor(
                 FuzzyReflection.fromClass(MinecraftReflection.getMinecraftServerClass())
                         .getMethodByReturnTypeAndParameters("getAdvancements", advancementDataWorldClass)
@@ -66,7 +69,10 @@ public class AdvancementsPacketHandler extends PacketHandler {
         if (MinecraftVersion.NETHER_UPDATE.atOrAbove()) { // 1.16+
             // MC 1.16+
             // Loading of achievements requires an AdvancementDataWorld method
-            ADVANCEMENT_DATA_PLAYER_LOAD_FROM_ADVANCEMENT_DATA_WORLD_METHOD = Accessors.getMethodAccessor(advancementDataPlayerClass, "reload", advancementDataWorldClass);
+            ADVANCEMENT_DATA_PLAYER_LOAD_FROM_ADVANCEMENT_DATA_WORLD_METHOD = Accessors.getMethodAccessor(
+                    FuzzyReflection.fromClass(advancementDataPlayerClass)
+                            .getMethodByReturnTypeAndParameters("reload", void.class, advancementDataWorldClass)
+            );
         } else {
             // MC 1.12-1.15
             // Loading of achievements only needs the method to be called without any parameters